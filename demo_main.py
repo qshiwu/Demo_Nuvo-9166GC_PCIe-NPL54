9b8ecb536317ecc4b2b959bcdd2807ae5fc9ad43
kaichu wu <kaichu.wu@neousys-tech.com>
2025-02-26 16:56:27 +0800

    [FEAT] putting 2x video streamming + 1x yolov8 in threads mode

--- demo_main.py
+++ demo_main.py
@@ -11,12 +11,9 @@ from ultralytics import YOLO
 # Download YOLOv8n (nano version)
 model = YOLO("yolov8n.pt")  # This will automatically download the model if not found locally
 
-
 # Queue to share frames between threads (size=1 ensures old frames are dropped)
 frame102_queue = queue.Queue(maxsize=1)
 frame103_queue = queue.Queue(maxsize=1)
-
-
 
 cap102 = cv2.VideoCapture('/dev/video102')
 cap103 = cv2.VideoCapture('/dev/video103')
@@ -31,7 +28,9 @@ if not cap102.isOpened() or not cap103.isOpened():
 else:
     # Get 1st Frame
     ret103, frame103 = cap103.read()
+    annotated_frame103 = frame103
     ret102, frame102 = cap102.read()
+    annotated_frame102 = frame102
     
     # Get the primary monitor's resolution    
     monitor = get_monitors()[0]  # Assumes the first monitor is the primary one
@@ -117,7 +116,6 @@ def display_canvas():
         time.sleep(0.1)  # Avoid high CPU usage
     
     
-    
 def display_frame102():
     global frame102, running, frame102_queue
     while running and cap102.isOpened():        
@@ -143,10 +141,21 @@ def display_frame103():
         
         # Add frame to queue (drop old frames if needed)
         if not frame103_queue.full():
-            frame103_queue.put(frame102)
+            frame103_queue.put(frame103)
 
 
-
+def display_annoted_frame103():
+    global running, frame103_queue, annotated_frame103
+    while running:     
+        if not frame103_queue.empty():            
+            frame = frame103_queue.get()            
+            # Run YOLOv8 on the frame103 (only person detection)
+            results103 = model(frame103, classes=[0])
+            # Show results
+            annotated_frame103 = results103[0].plot()
+        time.sleep(0.006)
+    
+   
 # Create and Start Threads
 # thread_display_canvas = threading.Thread(target=display_canvas)
 # thread_display_canvas.start()
@@ -156,6 +165,9 @@ thread_display_frame102.start()
 
 thread_display_frame103 = threading.Thread(target=display_frame103)
 thread_display_frame103.start()
+
+thread_display_annoted_frame103 = threading.Thread(target=display_annoted_frame103)
+thread_display_annoted_frame103.start()
 
 
 # Display the Canvas
@@ -170,7 +182,7 @@ while True:
     
     # frame 103
     canvas[monitor.height//2:monitor.height, 0:monitor.width//2] = cv2.resize(frame103, (monitor.width//2, monitor.height//2))
-    # canvas[monitor.height//2:monitor.height, monitor.width//2:monitor.width] = cv2.resize(annotated_frame103, (monitor.width//2, monitor.height//2))
+    canvas[monitor.height//2:monitor.height, monitor.width//2:monitor.width] = cv2.resize(annotated_frame103, (monitor.width//2, monitor.height//2))
 
     cv2.line(canvas, (0, monitor.height//2), (monitor.width, monitor.height//2), (200, 200, 200), 1)
     cv2.line(canvas, (monitor.width//2, 0), (monitor.width//2, monitor.height), (200, 200, 200), 1)
@@ -182,13 +194,13 @@ while True:
         running = False
         break
     
-    
 
 
 # Wait for Threads to Finish
 # thread_display_canvas.join()
 thread_display_frame102.join()
 thread_display_frame103.join()
+thread_display_annoted_frame103.join()
 
 
 # When everything done, release the cap102 and cap103
