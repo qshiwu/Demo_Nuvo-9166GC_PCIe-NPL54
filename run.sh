3a984d16cf345401900b9483289c302878e66f9a
kaichu wu <kaichu.wu@neousys-tech.com>
2025-03-05 08:57:05 +0800

    [FEAT] v0.9 release

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@
 sudo chown $(whoami) /sys/class/hwmon/hwmon1/pwm1_enable
 sudo chown $(whoami) /sys/class/hwmon/hwmon1/pwm1
 echo "1" >  /sys/class/hwmon/hwmon1/pwm1_enable
+
+# Manually change the fan speed, 0 to 255 (max)
 echo "70" > /sys/class/hwmon/hwmon1/pwm1
 
 cd /home/dvt/Desktop/NPL54_ES2/app/NPL54Capture/
@@ -17,9 +19,11 @@ sleep 5
 #gnome-terminal -- ffplay /dev/video102
 #gnome-terminal -- ffplay /dev/video103
 
+sudo pkill python3
+
 gnome-terminal -- bash -c "cd /home/dvt/Desktop/Demo_Nuvo-9166GC_PCIe-NPL54; python3 demo_main_102.py" 
 
-sleep 5 
+sleep 0.1
 
 gnome-terminal -- bash -c "cd /home/dvt/Desktop/Demo_Nuvo-9166GC_PCIe-NPL54; python3 demo_main_103.py" 
 
